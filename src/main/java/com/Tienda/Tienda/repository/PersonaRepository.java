2f682f286225c91abf482932c1f0b9a2e58b47b0
JeffreyMojica <josuejeff17@gmail.com>
2023-02-17 19:38:41 -0600

    Quinta Semana

---
+++ src/main/java/com/Tienda/Tienda/repository/PersonaRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.Tienda.repository;
+
+
+import com.Tienda.Tienda.entity.Persona;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Jeffrey Mojica S
+ */
+public interface PersonaRepository extends CrudRepository<Persona,Long>{
+    
+}
