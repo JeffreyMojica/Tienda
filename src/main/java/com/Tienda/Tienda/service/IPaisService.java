2f53dd49cc3f854bb20645ffca05c39e5d3173b8
JeffreyMojica <josuejeff17@gmail.com>
2023-02-17 19:40:02 -0600

    Quinta Semana

---
+++ src/main/java/com/Tienda/Tienda/service/IPaisService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Tienda.Tienda.service;
+
+import com.Tienda.Tienda.entity.Pais;
+import java.util.List;
+
+/**
+ *
+ * @author Jeffrey Mojica S
+ */
+public interface IPaisService {
+    public List<Pais> listCountry(); //Definimos el metodo
+    
+    
+}
